5a7e426873714d4b50c52a8ed7b03ec570ad8a13
stacey.tadrus@gmail.com <stacey.tadrus@gmail.com>
2025-04-28 17:17:08 -0500

    Practice

--- stringUtils.js
+++ stringUtils.js
@@ -6,19 +6,20 @@ uppercase.
 ■ reverse(str): Reverses the characters in a string. 
 ■ contains(str, substr): Checks if a substring exists within a 
 string and returns true or false. */
-
 function capitalize(str){
-    return str.toUpperCase().charAT(0);//converts the first letter of a string to uppercase.
+    return str.toUpperCase();//converts the first letter of a string to uppercase.
 }
-function reverse(str){
-    return str.splt(str).reverse(str).join(str);// reverse and joins the string
+function reverseStr(str){
+    return str.split("").reverse().join("");// reverse and joins the string
 }
 function contains(str,substr){
-    if (str.substr = true)
-        return substr; 
+    if (str.length = substr){
+        return true; 
+    } else{
+        return false;
+    }
+        
 
 }
 
-    // module.exports = {capitalize};
-    // module.exports = {reverse};
-    // module.exports = {contains};
+    module.exports = {capitalize,reverseStr,contains};
